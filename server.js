142a5e0bb4781ac56ca4c719c6edffe01bb64fe6
JoeyDeckers <deckersjoey@gmail.com>
2020-09-15 21:35:21 +0200

    Added node server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-const express = require("express");
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log(`Working on: ${process.env.PORT || 3000}`);
+})
